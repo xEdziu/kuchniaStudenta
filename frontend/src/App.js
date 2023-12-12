79c6af2d3d0b7a79d13f80498ac5bdfd08213100
Adrian Goral <adrian.goral@gmail.com>
2023-12-12 01:11:47 +0100

    Update .env file and .gitignore, and modify App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Adrian Goral & Jakub Przybysz
         </p>
         <p className="App-link">
           {message}
