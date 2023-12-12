428547b1662643e5c067fba177fb26e3897da4b5
Adrian Goral <adrian.goral@gmail.com>
2023-12-12 01:06:03 +0100

    Update backend and frontend code

--- backend/src/Controller/HelloController.php
+++ backend/src/Controller/HelloController.php
@@ -3,16 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-    #[Route('/hello', name: 'app_hello')]
-    public function index(): JsonResponse
+    #[Route('/api/hello', name: 'hello')]
+    public function index(): Response
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => 'Welcome to our great page! - api/hello',
             'path' => 'src/Controller/HelloController.php',
         ]);
     }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://192.168.66.92:8001/api/hello');
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('Error fetching data from backend:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-link">
+          {message}
+        </p>
       </header>
     </div>
   );
 }
-
+//localhost:8000/api/hello
 export default App;
