83bc665595ea1171325b5f3040b781cca49453b3
Adrian Goral <adrian.goral@gmail.com>
2023-12-29 23:33:09 +0100

    Getting ready for recaptcha

---
+++ backend/src/Service/RecaptchaService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service;
+
+class RecaptchaService
+{
+    private string $secretKey;
+    private string $verifyUrl = "https://www.google.com/recaptcha/api/siteverify";
+
+    public function __construct(string $response, string $serverKey,)
+    {
+        $this->secretKey = $serverKey;
+    }
+
+    public function verify(string $response): bool
+    {
+        $res = file_get_contents($this->verifyUrl . '?secret=' . $this->secretKey . '&response=' . $response);
+        $responseData = json_decode($res);
+        return $responseData->score >= 0.5;
+    }
+
+}
